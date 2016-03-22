142048cc739fd81a367511177306036d4fa249eb
kevinzou <kaenry@163.com>
2016-03-22 15:28:06 +0800

    update logging, add datasource pool and active dev properties

--- src/main/java/com/zf/Application.java
+++ src/main/java/com/zf/Application.java
@@ -1,7 +1,8 @@
 package com.zf;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
@@ -9,14 +10,16 @@ import java.util.Arrays;
 
 @SpringBootApplication
 public class Application {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(Application.class);
     
     public static void main(String[] args) {
         ApplicationContext ctx = SpringApplication.run(Application.class, args);
-        System.out.println("Let's inspect the beans provided by Spring Boot:");
+        LOGGER.debug("Let's inspect the beans provided by Spring Boot:");
         String[] beanNames = ctx.getBeanDefinitionNames();
         Arrays.sort(beanNames);
         for (String beanName : beanNames) {
-            System.out.println(beanName);
+            LOGGER.debug("Beans:", beanName);
         }
     }
 }
