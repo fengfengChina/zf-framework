0ffcb2d824834ed9f510745db180c8d60f807409
ZengFeng <305737657@qq.com>
2016-03-28 00:00:52 +0800

    add goods controller

---
+++ src/main/java/com/zf/dao/GoodsCommentRepo.java
@@ -0,0 +1,10 @@
+package com.zf.dao;
+
+/**
+ * GoodsCommentRepo
+ *
+ * @author zf
+ * @date 16/3/27
+ */
+public class GoodsCommentRepo {
+}
---
+++ src/main/java/com/zf/dao/GoodsDetailRepo.java
@@ -0,0 +1,10 @@
+package com.zf.dao;
+
+/**
+ * GoodsDetailRepo
+ *
+ * @author zf
+ * @date 16/3/27
+ */
+public class GoodsDetailRepo {
+}
